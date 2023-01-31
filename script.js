404ae495254adf9c37bd6b282cd18db0d3059bd1
Your Name <you@example.com>
2023-01-31 16:18:36 +0300

    new cars

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const slides = document.querySelectorAll('.slide')
 
-for(const slide of slides) {
+const Cars = (activeSlides = 0) => {
+  const slides = document.querySelectorAll('.slide')
+
+  slides[activeSlides].classList.add('active')
+
+  for(const slide of slides) {
   slide.addEventListener('click', () => {
     clearActiveClasses()
     slide.classList.add('active')
@@ -12,3 +16,5 @@ const clearActiveClasses = () => {
     slide.classList.remove('active')
   })
 }
+}
+Cars(0)
